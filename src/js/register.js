2889fe17d4bf116e6f81fdced22c8c8068a88983
Alexander Hilding <hildingx@gmail.com>
2024-05-02 13:14:29 +0200

    Lagt till funktionalitet för att lägga till ny användare.

--- src/js/register.js
+++ src/js/register.js
@@ -1,12 +1,50 @@
-//Registrera
-document.getElementById('registerForm').addEventListener('submit', function(event) {
+document.getElementById('registerForm').addEventListener('submit', async function(event) {
     event.preventDefault();
-    const username = document.getElementById('registerUsername').value;
-    const password = document.getElementById('registerPassword').value;
-    const firstName = document.getElementById('registerFirstName').value;
-    const lastName = document.getElementById('registerLastName').value;
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    const firstname = document.getElementById('firstname').value.trim();
+    const lastname = document.getElementById('lastname').value.trim();
     
-    console.log('Register with:', username, password, firstName, lastName);
+    const errorEl = document.getElementById('formError');
+    const expAddedEl = document.getElementById('expAdded');
+    const form = document.getElementById('registerForm');
 
+    //Validera input för att kontrollera att inga fält är tomma
+    if (!username || !password || !firstname || !lastname) {
+        errorEl.innerHTML = 'Alla fält måste fyllas i. <br> Vänligen kontrollera dina inmatningar.';
+        errorEl.style.display = 'block';
+        return;
+    }
 
+    //Objekt med insamlad användardata
+    const userData = {
+        username: username,
+        password: password,
+        firstname: firstname,
+        lastname: lastname
+    };
+
+    try {
+        const response = await fetch('https://dt207g-mom4-1.onrender.com/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData)
+        });
+        
+        if (!response.ok) {
+            errorEl.innerHTML = 'Användare kunde inte läggas till';
+            errorEl.style.display = 'block';
+        } else {
+            expAddedEl.innerHTML = 'Användare tillagd';
+            expAddedEl.style.display = 'block';
+            //Rensa formuläret
+            form.reset();
+        }
+    } catch (error) {
+        console.error('Det gick inte att lägga till användare:', error);
+        errorEl.textContent = 'Användare kunde inte läggas till.';
+        errorEl.style.display = 'block';
+    }
 });
