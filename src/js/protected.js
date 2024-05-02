3b937667701444e7fee83865c433514f7e53783a
Alexander Hilding <hildingx@gmail.com>
2024-05-02 15:11:44 +0200

    provar ny funktion

--- src/js/protected.js
+++ src/js/protected.js
@@ -3,7 +3,31 @@ window.onload = function() {
     if (!token) {
       window.location.href = '/index.html'; //Omdirigera till login-sidan om ingen token finns
     }
-    console.log("Token existerar" + token);
 }
 
-console.log("test");
+async function fetchAccountDetails() {
+  const url = 'https://dt207g-mom4-1.onrender.com/api/user';
+  const token = localStorage.getItem('authToken');
+  try {
+      const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+          }
+      });
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      
+      const accountDiv = document.getElementById('accountDetails');
+      accountDiv.innerHTML = `First Name: ${data.firstname}<br>Last Name: ${data.lastname}`;
+  } catch (error) {
+      console.error('Failed to fetch account details:', error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchAccountDetails();
+});
